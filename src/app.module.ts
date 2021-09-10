0bf855bece791cf1995c6ffcea0d25a8353178c7
Owen Zhong <zhenyanz@usc.edu>
2021-09-10 20:17:22 +0800

    enable ssr

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ViewModule } from './view/view.module';
 
 @Module({
   imports: [
@@ -16,8 +15,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       sortSchema: true,
     }),
     TypeOrmModule.forRoot(),
+    ViewModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/client/pages/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>Test App</div>
+}
+
+export default App;
